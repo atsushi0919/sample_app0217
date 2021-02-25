95bb41491d38c500fcd1dd1d6b5def874b511053
Atsushi Hatakeyama <hatake0919@yahoo.co.jp>
2021-02-26 06:05:20 +0900

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -14,13 +9,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -33,6 +28,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  # development環境のメール設定
+  host = "localhost:3000"                     # ローカル環境
+  config.action_mailer.default_url_options = { host: host, protocol: "http" }
 
   config.action_mailer.perform_caching = false
 
