886ff1bd8ea50e8050b0c1f64830681d1d132723
atsushi0919 <hatake0919@yahoo.co.jp>
2021-02-26 16:54:37 +0900

    list 13.58

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   # ログインユーザーのみ作成/削除できる
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     # formで送った値をmicropost_paramsで受け取り current_userに渡して代入
@@ -9,16 +10,26 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed.paginate(page: params[:page])
       render "static_pages/home"
     end
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_back(fallback_location: root_url)
+    #redirect_to request.referrer || root_url
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
