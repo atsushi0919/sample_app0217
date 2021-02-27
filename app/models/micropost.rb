b60af87c7db6db50db5f8c9e211c44d07f770eba
atsushi0919 <hatake0919@yahoo.co.jp>
2021-02-27 09:08:44 +0900

    list 13.64

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image # 投稿1件につき画像1件
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
